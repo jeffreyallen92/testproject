6aeca9de10b651863524349b339243d30077c34c
gweelay <jeffreyallen92@gmail.com>
2014-08-11 22:10:14 +0000

    Added rango app urls to django project urls

--- testproject/urls.py
+++ testproject/urls.py
@@ -9,4 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    # Added the rango app to mapped urls
+    url(r'^rango/', include('rango.urls')),
+
 )
